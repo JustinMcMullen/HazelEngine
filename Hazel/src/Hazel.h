9e9997cf525fa5f9d51e28fad1788a54a9a5cc3e
McJasper-PC\McJasper <mcjustinosu@gmail.com>
2021-04-09 22:06:58 +0200

    Rm dir and re-add to allow spdlog inclusion.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications only.
+
+#include "Hazel/Application.h"
+#include<stdio.h> //REMOVE
+
+// ----------Entry point -------
+#include "Hazel/EntryPoint.h"
